29b8971927a3f2264a2cc9d0912011cd0f4cfbde
jayaprakash <jayaprakash676@gmail.com>
2021-01-07 01:35:06 +0530

    Create SettingsDataRepository.php

---
+++ New/Model/SettingsDataRepository.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace News\New\Model;
+
+use News\New\Api\SettingsDataRepositoryInterface;
+use News\New\Model\SetingsDataInterface;
+use News\New\Model\SettingsDataFactory;
+use News\New\Model\ResourceModel\SettingsData\CollectionFactory;
+
+use Magento\Framework\Api\SearchCriteriaInterface;
+use Magento\Framework\
